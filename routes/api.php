d75d3193d5a184500f2f01495220de227578c4d2
mirzapurnandi <mirza.purnandi@gmail.com>
2023-02-12 11:01:38 +0700

    fix 1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -8,4 +9,13 @@ Route::group([
 ], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::group([
+    'middleware' => ['auth:api'], 'prefix' => 'v1'
+], function ($router) {
+    Route::get('category', [CategoryController::class, 'index']);
+    Route::post('category', [CategoryController::class, 'store']);
+    Route::put('category/{id}', [CategoryController::class, 'update']);
+    Route::delete('category/{id}', [CategoryController::class, 'destroy']);
 });
