bfefa289e2168470cba2577b7d57a36f63b252e1
mirzapurnandi <mirza.purnandi@gmail.com>
2023-02-12 11:28:16 +0700

    menambahkan Exception untuk handle error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,40 +2,56 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Response;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
-     */
+    use ApiResponser;
+
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof SurplusException) {
+            return $this->errorResponse($exception->message, $exception->status);
+        } else if ($exception instanceof MethodNotAllowedHttpException) {
+            $code = $exception->getStatusCode();
+            $message = "Method URL tidak diizinkan";
+            return $this->errorResponse($message, $code);
+        } else if ($exception instanceof NotFoundHttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+            return $this->errorResponse($message, $code);
+        } else if ($exception instanceof RouteNotFoundException) {
+            return $this->errorResponse('Jangan asal bro', Response::HTTP_INTERNAL_SERVER_ERROR);
+        } else if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse('Unauthenticated, token tidak ditemukan', Response::HTTP_UNAUTHORIZED);
+        }
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
     }
 }
